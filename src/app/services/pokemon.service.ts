0b59bdf357accadd4165cf2c154078690e80bf25
eduardoGDM <eduardogdiasmachado@gmail.com>
2023-07-13 23:26:42 -0300

    API type corrigida

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -5,19 +5,18 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class PokemonService {
-  pokemons = []; // ira receber todos os objetos separados dentro de um array vindo do require para utilizar em um component .
-
+  pokemons: { name: string, url: string }[] = []; // ira receber todos os objetos separados dentro de um array vindo do require para utilizar em um component .
+  //especificar o tipo de dados do objeto recebido da API !!!! NÃO ESQUECER!!!!
   constructor(private httpClient: HttpClient) {
     //conexão para a requisição http
     this.carregarPokemons();
   }
-  async carregarPokemons() {                     //necessario ser uma função assincrona para aguardar uma promise
-                                         
+  async carregarPokemons() {                     //necessario ser uma função assincrona para aguardar uma promise                               
     const require = await this.httpClient //armazena em require a espera do objeto vindo do metodo GET
-      .get<any>('https://pokeapi.co/api/v2/pokemon?limite=151') //metodo get recebendo <any> dados do link da API
+      .get<any>('https://pokeapi.co/api/v2/pokemon?limit=151') //metodo get recebendo <any> dados do link da API
       .toPromise(); //promise dos dados recebidos do método GET
 
     this.pokemons = require.results; //results foram os dados separados na URL dentro do total de dados, no caso 151
-    //console.log(this.pokemons);
+    console.log(this.pokemons);
   }
 }
